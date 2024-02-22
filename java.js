8e479556ff98515d983cb32b398ab5db3a9253fb
AgentGoDPrabesh <gurukule18@gmail.com>
2024-02-22 21:51:34 +0545

    firstpush

---
+++ java.js
@@ -0,0 +1,137 @@
+// console.log("hello");
+
+// let q="guuu";
+// console.log(q);
+
+// let a=100,b=5;
+// console.log(a*b);
+// console.log(a+b);
+
+// let x= "xx";
+// let y="xyz";
+// console.log(x);
+// console.log(y);
+
+// let x= "ram";
+// console.log(x);
+
+//   let myfirstname ="Prabesh ";
+// console.log(myfirstname);
+
+// let  @  ="Ram";
+// console.log(@);
+
+// let Prabesh_khadka ="prabesh";
+// console.log(Prabesh_khadka);
+
+// let $hari ="sidd";
+// console.log($hari);
+// let let="x";
+// console.log(let);
+
+// let name = "prabesh kkhadkaaa"
+// console.log(name)
+
+// let a="he is a boy";
+// let b="or a girl?";
+// console.log("who is he "+a+". "+b)
+
+// var name;
+// console.log(name);
+// name="prabesh";
+// console.log(name);
+// name="khadka";
+// console.log(name);
+
+// const name="prabesh";
+// console.log(name);
+// name="khadka";
+// console.log(name)
+
+// let a="apple";
+// console.log(a)
+// {
+// let a="ball"
+// console.log(a)
+// }
+// console.log(a)
+
+//  let x=3;
+//  let y=6;
+//  console.log(x+y)
+//  console.log("the sum of number is ",x+y);
+//  console.log("the sum of",x,"and",y,"is",x+y);
+
+
+
+
+
+// let x=12;
+// console.log((x>6)||(x>14));
+
+// let a="prabesh ";
+// let b=" khadka";
+// console.log(a + b);
+
+// let x=12;
+// if(x>10){
+  // console.log("ok")
+// }
+// else {
+  // console.log("not ok")
+// }
+
+// let num=3;
+// if(num%2==0){
+  // console.log("even");
+// }
+// else{
+  // console.log("odd");
+// }
+
+
+
+
+
+// let name="prabesh";
+// let age= 19;
+// let isVerified = true;
+// console.log(name);
+// console.log(age);
+// console.log(isVerified);
+
+// let like= 2;
+// like+=1; 
+// console.log(like);
+
+// let Age=17;
+// console.log(Age>18);
+
+// let age=17;
+// console.log(age==17);
+
+// let x="17";
+// let y="17";
+// console.log(x==y);
+
+// let num= 7;
+// if(num>0)
+// console.log("+ve");
+// else if(num<0)
+// console.log( "-ve");
+// else
+// console.log("zero");
+
+                                         // ------Object in java
+  // let person={
+    // name:"prabesh",
+    //  age:19,
+  // }
+  // console.log(typeof person);
+  // console.log(person.name);
+  // console.log(person["age"]);
+
+
+
+
+
